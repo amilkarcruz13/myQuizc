4e00d99508f05bcb6b2a146b4e2c9e14cd8f7a7c
amilkarcruz13 <amilkar.developer@gmail.com>
2020-10-07 19:06:24 -0400

    My Quizc -  Amilkar Cruz Herbas

---
+++ quizc/model/validations.py
@@ -0,0 +1,79 @@
+import datetime
+from enum import Enum
+
+
+class RequiredValidator(object):
+    MESSAGE = "This question is required"
+
+    def validate(self, value, condition_value, errors):
+        if value == "":
+            errors.append(self.MESSAGE)
+
+
+class DateValidator(object):
+    DATE_FORMAT = '%m/%d/%Y'
+    MESSAGE = "The date format is invalid, it should have the format mm/dd/yyyy"
+
+    def validate(self, value, condition_value, errors):
+        try:
+            datetime.datetime.strptime(value, '%d/%m/%Y')
+        except ValueError:
+            errors.append(self.MESSAGE) 
+
+class MinValidator(object):
+    MESSAGE = "The value must be greater than {min_value}"
+
+    def validate(self, value, condition_value, errors):
+        if value < condition_value:
+            errors.append(self.MESSAGE.format(min_value=condition_value))
+
+
+
+class MinLengthValidator(object):
+    MESSAGE = "The value length must be less than {max_length}"
+
+    def validate(self, value, condition_value, errors):
+        if len(value) < condition_value:
+            errors.append(self.MESSAGE.format(max_length=condition_value))
+
+class MaxLengthValidator(object):
+    MESSAGE = "The value length must be less than {max_length}"
+
+    def validate(self, value, condition_value, errors):
+        if len(value) > condition_value:
+            errors.append(self.MESSAGE.format(max_length=condition_value))
+
+class OnlyUpperCaseValidator(object):
+    MESSAGE = "The text format is invalid, it should be UPPERCASE"
+
+    def validate(self, value, condition_value, errors):
+        if value.isupper() == False:
+            errors.append(self.MESSAGE)
+
+class MaxValidator(object):
+    MESSAGE = "The value must be less than {max_value}"
+
+    def validate(self, value, condition_value, errors):
+        if value > condition_value:
+            errors.append(self.MESSAGE.format(max_value=condition_value))
+    
+
+class ValidatorType(Enum):
+    REQUIRED = (1, RequiredValidator())
+    DATE = (2, DateValidator())
+    MIN = (3, MinValidator())
+    MIN_LENGTH = (4, MinLengthValidator())
+    MAX_LENGTH = (5, MaxLengthValidator())
+    ONLY_UPPERCASE = (6, OnlyUpperCaseValidator())
+    MAX = (7, MaxValidator())
+
+    def __init__(self, code, validator_instance):
+        self.code = code
+        self.validator_instance = validator_instance
+
+    @staticmethod
+    def get_validator(validator_code):
+        for validator in ValidatorType:
+            if validator.code == validator_code or str(validator.code) == validator_code:
+                return validator.validator_instance
+        return None
